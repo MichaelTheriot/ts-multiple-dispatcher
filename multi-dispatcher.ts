e099621972fbd9a4f8c7336243aec1a1fa0e5c5b
Michael Theriot <michael.lee.theriot@gmail.com>
2025-07-27 00:45:49 -0500

    add support for constructors

--- multi-dispatcher.ts
+++ multi-dispatcher.ts
@@ -3,6 +3,15 @@ type TypeGuardArray<T extends any[]> = {
     [K in keyof T]: TypeGuard<T[K]>;
 };
 
+type Callable = (...args: any[]) => any;
+type Constructable = new (...args: any[]) => any;
+
+type FunctionParameters<T extends Function> = T extends Callable
+    ? Parameters<T>
+    : T extends Constructable
+        ? ConstructorParameters<T>
+        : never;
+
 const testSignature = (args: unknown[], guards: TypeGuard<unknown>[], fn: Function) => {
     // test signature when there are matching number of arguments
     if (args.length === guards.length) {
@@ -11,7 +20,7 @@ const testSignature = (args: unknown[], guards: TypeGuard<unknown>[], fn: Functi
 
     // test signature when there are variadic arguments, using last guard for all rest parameters
     if (args.length >= guards.length - 1 && guards.length === fn.length + 1) {
-        // check explict parameters
+        // check explicit parameters
         for (let i = 0; i < fn.length; i++) {
             if (!guards[i](args[i])) {
                 return false;
@@ -32,7 +41,7 @@ const testSignature = (args: unknown[], guards: TypeGuard<unknown>[], fn: Functi
     return false;
 };
 
-class MultipleDispatcher<T extends (...args: any[]) => any> {
+export class DispatchResolver<T extends Function> {
     #defaultFn: T;
     #dispatches: [TypeGuard<unknown>[], T][] = [];
 
@@ -40,19 +49,29 @@ class MultipleDispatcher<T extends (...args: any[]) => any> {
         this.#defaultFn = defaultFn;
     }
 
-    register(fn: T, ...guards: TypeGuardArray<Parameters<T>>) {
+    register(fn: T, ...guards: TypeGuardArray<FunctionParameters<T>>) {
         this.#dispatches.push([guards, fn]);
     }
 
-    dispatch(...args: Parameters<T>): ReturnType<T> {
+    resolve(...args: any[]) {
         for (const [guards, fn] of this.#dispatches) {
             if (testSignature(args, guards, fn)) {
-                return fn(...args);
+                return fn;
             }
         }
 
-        return this.#defaultFn(...args);
+        return this.#defaultFn;
     }
 }
 
-export default MultipleDispatcher;
+export class FunctionDispatcher<T extends Callable> extends DispatchResolver<T> {
+    dispatch(...args: any[]): ReturnType<T> {
+        return (this.resolve(...args))(...args);
+    }
+}
+
+export class ConstructorDispatcher<T extends Constructable> extends DispatchResolver<T> {
+    construct(...args: any[]): InstanceType<T> {
+        return new (this.resolve(...args))(...args);
+    }
+}
